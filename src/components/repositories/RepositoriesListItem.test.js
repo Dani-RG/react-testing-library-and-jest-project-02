c9015c9cb169c69647e04e85641847ac9d3d09bd
Daniel-RG <21danielrondong@gmail.com>
2024-01-14 18:05:58 +0100

    fix 2nd bug complete

--- src/components/repositories/RepositoriesListItem.test.js
+++ src/components/repositories/RepositoriesListItem.test.js
@@ -1,21 +1,16 @@
 import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter } from "react-router-dom";
 import RepositoriesListItem from "./RepositoriesListItem";
-
-jest.mock("../tree/FileIcon.js", () => {
-  // replace content of FileIcon.js
-  return () => {
-    return "File Icon Component";
-  };
-});
 
 function renderComponent() {
   const repository = {
     full_name: "facebook/react",
     language: "Javascript",
-    description: "A JS library",
-    owner: "Facebook",
-    name: "React",
+    description: "A js library",
+    owner: {
+      login: "facebook",
+    },
+    name: "react",
     html_url: "https://github.com/facebook/react",
   };
   render(
@@ -23,8 +18,37 @@ function renderComponent() {
       <RepositoriesListItem repository={repository} />
     </MemoryRouter>
   );
+
+  return { repository };
 }
 
 test("shows a link to the github homepage for this repository", async () => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: "Javascript" });
+
+  const link = screen.getByRole("link", {
+    name: /github repository/i,
+  });
+  expect(link).toHaveAttribute("href", repository.html_url);
+});
+
+test("shows a fileicon with the appropiate icon", async () => {
   renderComponent();
+
+  const icon = await screen.findByRole("img", { name: "Javascript" });
+
+  expect(icon).toHaveClass("js-icon");
+});
+
+test("shows a link to the code editor page", async () => {
+  const { repository } = renderComponent();
+
+  await screen.findByRole("img", { name: "Javascript" });
+
+  const link = await screen.findByRole("link", {
+    name: new RegExp(repository.owner.login),
+  });
+
+  expect(link).toHaveAttribute("href", `/repositories/${repository.full_name}`);
 });
